02e9ac7a539a354752b29f160573fc7fa60458c4
anhkiet307 <kietlulk6969@gmail.com>
2025-10-13 08:56:43 +0700

    done : user API

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -3,7 +3,7 @@ const API_CONFIG = {
   // Base URL cho tất cả API calls
   BASE_URL:
     process.env.REACT_APP_API_BASE_URL ||
-    "https://6d5f64c495e1.ngrok-free.app/api",
+    "https://506d44d86f44.ngrok-free.app/api",
 
   // Các endpoint cụ thể
   ENDPOINTS: {
